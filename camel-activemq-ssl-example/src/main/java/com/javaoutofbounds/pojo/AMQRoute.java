6b34e38942ee6b36a4d4a830c9eeb89ce791d23d
Erfin Feluzy <efeluzy@redhat.com>
2020-01-13 15:31:20 +0700

    add readme

--- camel-activemq-ssl-example/src/main/java/com/javaoutofbounds/pojo/AMQRoute.java
+++ camel-activemq-ssl-example/src/main/java/com/javaoutofbounds/pojo/AMQRoute.java
@@ -8,15 +8,22 @@ public class AMQRoute extends RouteBuilder{
 	@Override
 	public void configure() throws Exception {
 
-		from("cxfrs:bean:cxfrsRestApiEndpoint")
-		.convertBodyTo(String.class, "UTF-8")
-		// Below line sends the message to AMQ queue JavaOutOfBoundsQueue
-		.to(ExchangePattern.InOnly, "activemq:queue:JavaOutOfBoundsQueue")
-		.log("Messsage sent to AMQ Queue :: ${body}")
-		// Below line reads the message from the AMQ queue JavaOutOfBoundsQueue
-		.from("activemq:queue:JavaOutOfBoundsQueue")
-		.log("Messsage read from AMQ Queue : ${body}")
-		.transform(simple("Hello ${body}, Welcome to JavaOutOfBounds.com"))
-		.convertBodyTo(String.class, "UTF-8");
+//		from("cxfrs:bean:cxfrsRestApiEndpoint")
+//		.convertBodyTo(String.class, "UTF-8")
+//		// Below line sends the message to AMQ queue JavaOutOfBoundsQueue
+//		.to(ExchangePattern.InOnly, "activemq:queue:JavaOutOfBoundsQueue")
+//		.log("Messsage sent to AMQ Queue :: ${body}")
+//		
+//		
+//		// Below line reads the message from the AMQ queue JavaOutOfBoundsQueue
+//		.from("activemq:queue:JavaOutOfBoundsQueue")
+//		.log("Messsage read from AMQ Queue : ${body}")
+//		.transform(simple("Hello ${body}, Welcome to JavaOutOfBounds.com"))
+//		.convertBodyTo(String.class, "UTF-8");
+		
+		from("timer:foo?fixedRate=true&period=1000")
+			.log("timer on ${header.firedTime}")
+			.setBody(simple("Hello from timer at ${header.firedTime}"))
+			.to("activemq:queue:JavaOutOfBoundsQueue");
 	}
 }
